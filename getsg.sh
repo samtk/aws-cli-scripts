b7760c8b3da67051114a9344ffb428c9f34d588f
EC2 Default User <ec2-user@ip-172-31-36-20.ec2.internal>
2018-11-01 16:28:31 +0000

    Added script to collect info on security groups

---
+++ getsg.sh
@@ -0,0 +1,15 @@
+
+
+aws ec2 describe-security-groups | tail -n +2 | head -n -1 > input.json
+
+echo 'VpcId, GroupId, GroupName, Description, TagName, From Port, To Port, Protocol, Source (IPv4), Source (IPv6), Description, From Port, To Port, Protocol, Destination, Description' > securityGroups.csv 
+
+echo '[' > input.json
+aws ec2 describe-security-groups | tail -n +3 | head -n -1 >> input.json
+
+#Add items with tags
+jq -r '.[] | select(.Tags != null) | .VpcId as $vpcid | .GroupId as $groupid | .GroupName as $groupname | .Description as $description | .IpPermissions[].FromPort as $fromport | .IpPermissions[].ToPort as $toport | .IpPermissions[].IpProtocol as $ipprotocol | .IpPermissions[].IpRanges[].CidrIp as $sourceipv4 | .IpPermissions[].Ipv6Ranges[].CidrIpv6 as $sourceipv6 | .IpPermissions[].IpRanges[].Description as $ipv4description | .IpPermissionsEgress[].FromPort as $outfromport | .IpPermissionsEgress[].ToPort as $outtoport | .IpPermissionsEgress[].IpProtocol as $outprotocol | .IpPermissionsEgress[].IpRanges[].CidrIp as $ipv4outbound |.IpPermissionsEgress[].IpRanges[].Description as $outdescription |.Tags[].Key as $key | ([$vpcid, $groupid, $groupname, $description, $key ,$fromport, $toport, $ipprotocol, $sourceipv4, $sourceipv6, $ipv4description, $outfromport, $outtoport, $outprotocol,$ipv4outbound, $outdescription] | @csv)' input.json | sort -u >> securityGroups.csv
+
+#Add items without tags
+jq -r '.[] | select(.Tags == null) | .VpcId as $vpcid | .GroupId as $groupid | .GroupName as $groupname | .Description as $description | .IpPermissions[].FromPort as $fromport | .IpPermissions[].ToPort as $toport | .IpPermissions[].IpProtocol as $ipprotocol | .IpPermissions[].IpRanges[].CidrIp as $sourceipv4 | .IpPermissions[].Ipv6Ranges[].CidrIpv6 as $sourceipv6 | .IpPermissions[].IpRanges[].Description as $ipv4description | .IpPermissionsEgress[].FromPort as $outfromport | .IpPermissionsEgress[].ToPort as $outtoport | .IpPermissionsEgress[].IpProtocol as $outprotocol | .IpPermissionsEgress[].IpRanges[].CidrIp as $ipv4outbound |.IpPermissionsEgress[].IpRanges[].Description as $outdescription | ([$vpcid, $groupid, $groupname, $description, null,$fromport, $toport, $ipprotocol,$sourceipv4, $sourceipv6, $ipv4description, $outfromport, $outtoport, $outprotocol,$ipv4outbound, $outdescription] | @csv)' input.json | sort -u >> securityGroups.csv
+
